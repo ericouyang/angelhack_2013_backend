698e8f4c6a84690a330dd40ace05ca8ff6f5bcc8
Eric Ouyang <contact@ericouyang.com>
2013-06-16 11:15:03 -0700

    splash page tweaks

--- fuel/app/views/welcome/index.php
+++ fuel/app/views/welcome/index.php
@@ -4,18 +4,23 @@
 	<meta charset="utf-8">
 	<title>Luup.me</title>
 	<?php echo Asset::css('bootstrap.css'); ?>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300' rel='stylesheet' type='text/css'>
 	<style>
 		body { 
+		  font-family: 'Open Sans', sans-serif;
 		  color: #fff;
 		  background-color: #27AE60;
 		  text-align: center;
+		  font-weight: 300;
 	  }
 	  .container {
 	    margin-top: 150px;
 	  }
 	  p {
+	    font-family: 'Open Sans', sans-serif;
 	    line-height: 40px;
 	    font-size: 20px;
+	    margin-top: 20px;
 	  }
 	  a {
 	    color: #fff;
@@ -24,11 +29,17 @@
 	    color: #fff;
 	    text-decoration: underline;
 	  }
+	  h3 {
+	    font-weight: 300;
+	    font-size: 36px;
+	    line-height: 46px;
+	  }
 	</style>
 </head>
 <body>
 	<div class="container">
       <h1><img src="/assets/img/logo.png" /></h1>
+      <h3>Real world shopping <br> with a digital touch</h3>
       <p class="login">Our client? <a href="/admin">Login &raquo;</a></p>
 	</div>
 </body>
