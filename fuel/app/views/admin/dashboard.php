9dd4f73f69f3da10eec8b060de82487462d2f28e
dashuai99@gmail.com <dashuai99@gmail.com>
2013-06-16 08:56:57 -0700

    rebranded from <insert questionable project name here> to Luup

--- fuel/app/views/admin/dashboard.php
+++ fuel/app/views/admin/dashboard.php
@@ -1,24 +1,24 @@
 <div class="hero-unit">
-	<h1>Welcome to &lt;insert questionable project name here&gt;!</h1>
+	<h1>Welcome to Luup!</h1>
 	<p>Get started by managing business inventory!</p>
 	<p><a class="btn btn-primary btn-large" href="/item/index.php">View Inventory</a></p>
 </div>
 <div class="row">
 	<div class="span4">
 		<h2>How It Works</h2>
-		<p>&lt;insert questionable project name here&gt; lets your customers use their smartphones to quickly purchase your goods!</p>
+		<p>Luup lets your customers use their smartphones to quickly purchase your goods!</p>
 		<p>To start, you can add or manage your inventory <a href="/item/index.php">here</a>.</p>
 		<p>Once your customers start making transactions, you can keep track of them <a href="/transaction/item/index.php">here</a>!</p>
 	</div>
 	<div class="span4">
 		<h2>Mobile App</h2>
-		<p>Your customers can download the &lt;insert questionable project name here&gt; app for their iOS devices! (Android coming soon.)</p>
+		<p>Your customers can download the Luup app for their iOS devices! (Android coming soon.)</p>
 		<p>A customer checks in to your store, which lets them start scanning barcodes and adding their items to a digital cart.</p>
 		<p>When they've finished shopping, they go to the cashier, where they can pay by regular means or with PayPal. After showing the PayPal receipt to the cashier, they're off to continue with their lives!</p>
 	</div>
 	<div class="span4">
 		<h2>Why?</h2>
 		<p>For all of the changes in the past few decades, shopping at a store is still the same. And it's annoying. Stores have lines and out of stock items that drive away customers.</p>
-		<p>Other self-checkout software exists, but &lt;insert questionable project name here&gt; was designed to be easy for consumers to use and for stores to implement, so it can be used anywhere!</p>
+		<p>Other self-checkout software exists, but Luup was designed to be easy for consumers to use and for stores to implement, so it can be used anywhere!</p>
 	</div>
 </div>
